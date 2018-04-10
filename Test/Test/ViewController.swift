fbbae1491bb7b6b76e8dff7384d4622407ed1ca6
Mousty <melabbas@deakin.edu.au>
2018-04-10 17:03:50 +1000

    last commit on Monday session

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func clickbtn(_ sender: UIButton) {
+        sender.setImage(UIImage(named: "xplayer1"), for: .normal)
+    }
+    
 }
 
